45fee6d31daa9278a22bdc02ff1f4286f2493cd3
jess darko <sofronova.1999@mail.ru>
2023-10-04 18:19:19 +0400

    tried to fix code climate issue

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,12 +6,17 @@ const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 const isEven = () => {
   console.log('Welcome to the Brain Games!');
+
   const userName = readlineSync.question('May I have your name? ');
   const result = `Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`;
+
   console.log(result);
+
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = getRandomInt(100);
+
     console.log(`Question: ${randomNumber}`);
+
     const answer = readlineSync.question('Your answer: ');
     if (((randomNumber % 2 === 0) && (answer === 'yes')) || ((randomNumber % 2 !== 0) && (answer === 'no'))) {
       console.log('Correct!');
@@ -21,7 +26,9 @@ const isEven = () => {
       return endOfGame;
     }
   }
+
   const happyEnd = `Congratulations, ${userName}!`;
+
   return happyEnd;
 };
 
