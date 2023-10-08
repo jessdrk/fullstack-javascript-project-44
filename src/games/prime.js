2e4c1982ca2488f45afaf4c34b24e2a93e51f75b
jess darko <sofronova.1999@mail.ru>
2023-10-09 01:37:23 +0400

    add check prime to zero

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,22 @@
 import getRandomInt from '../functions/randomInt.js';
 
+const numberIsNotZero = () => {
+  let number;
+
+  while (number === 0 || number === undefined) {
+    number = getRandomInt(100);
+  }
+
+  return number;
+};
+
 const isPrime = (num) => {
   let result;
+  if (num === 1) {
+    return 'yes';
+  } if (num === 2) {
+    return 'no';
+  }
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
       return 'no';
@@ -14,7 +29,7 @@ const isPrime = (num) => {
 
 const getDataOfPrime = () => {
   const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const question = getRandomInt(100);
+  const question = numberIsNotZero();
   const correctAnswer = isPrime(question);
 
   return [question, correctAnswer, description];
